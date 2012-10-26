ae56104e0e3bfc1c54967e2e783bb86a4bda5b86
Martin Kubat <martin.kubat@coex.cz>
2012-10-26 13:40:22 +0200

    clean

--- pyro_api/management/commands/run_api.py
+++ pyro_api/management/commands/run_api.py
@@ -5,8 +5,11 @@ import Pyro4
 
 from django.core.management.base import NoArgsCommand
 
-from pyro_api.settings import URI_PORT, URI_ID
-from pyro_api.api import DocumentAPI
+from pyro_api.settings import URI_PORT, URI_ID, HMAC_KEY, SERVER_IP
+
+################################################################################
+
+Pyro4.config.HMAC_KEY = HMAC_KEY
 
 ################################################################################
 
@@ -22,9 +25,12 @@ class Command(NoArgsCommand):
         start API
     """
     def handle_noargs(self, *args, **kwargs):
-        daemon = Pyro4.Daemon(port=URI_PORT)
-        # ns = Pyro4.locateNS()
+
+        from pyro_api.api import DocumentAPI
+
+        daemon = Pyro4.Daemon(port=URI_PORT, host=SERVER_IP)
+        # ns = Pyro4.locateNS()  # DEPRECATED
         uri = daemon.register(DocumentAPI(), URI_ID)
-        # ns.register(DAEMON_NAME, uri)
+        # ns.register(DAEMON_NAME, uri)  # DEPRECATED
         logger.info("Connected to Pyro NameServer %s" % uri)
         daemon.requestLoop()
--- run_ns.py
+++ run_ns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# DEPRECATED at now: use NameServer is you need
+
 # import Pyro4
 # import pyro_api  # load config
 
