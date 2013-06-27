f3f4ba307d4417570f2da7bb221a41639def0b2a
Martin Kubat <martin.kubat@coex.cz>
2013-06-27 09:52:59 +0200

    updated pyro for better and rock solid connection to port

--- pyro_api/management/commands/run_api.py
+++ pyro_api/management/commands/run_api.py
@@ -2,6 +2,7 @@
 
 import logging
 import Pyro4
+from time import sleep
 
 from django.core.management.base import NoArgsCommand
 
@@ -24,11 +25,40 @@ class Command(NoArgsCommand):
     """
         start API
     """
+
+    LOOP_LIMIT = 30
+
     def handle_noargs(self, *args, **kwargs):
+        print "START COMMAND"
 
         from pyro_api.api import DocumentAPI
 
-        daemon = Pyro4.Daemon(port=URI_PORT, host=SERVER_IP)
+        i = 0
+
+        daemon = None
+        while i < self.LOOP_LIMIT:
+            try:
+                # try to connect to address (port/ip)
+                daemon = Pyro4.Daemon(port=URI_PORT, host=SERVER_IP)
+            except Exception, e:
+                logger.error(str({
+                    "error": e,
+                    "loop": "%s/%s" % (i, self.LOOP_LIMIT)
+                }))
+                i += 1
+                sleep(3)
+
+            if daemon:
+                break
+
+        if not daemon:
+            logger.critical("Pyro connection has not been created!")
+            return
+
+        # Pyro4.socketutil.setReuseAddr(daemon.sock)
         uri = daemon.register(DocumentAPI(), URI_ID)
-        logger.info("Connected to Pyro NameServer %s" % uri)
-        daemon.requestLoop()
+        logger.info("Success connected to Pyro NameServer %s" % uri)
+        try:
+            daemon.requestLoop()
+        finally:
+            daemon.shutdown()
